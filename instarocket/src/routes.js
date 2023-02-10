68e5510feb49bfa265f087aa5bb85076bfae902e
“Lucas <lfmanofl@gmail.com>
2023-02-10 11:01:31 -0300

    projeto pausado

--- instarocket/src/routes.js
+++ instarocket/src/routes.js
@@ -1,18 +1,48 @@
-import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { View, Text, Button, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import logoImg from "../assets/logo.png";
+import send from "../assets/send.png";
+import New from "../src/pages/New";
 import Feed from "./pages/Feed";
-import New from "./pages/New";
+import { TouchableOpacity } from "react-native";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function MyStack() {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Feed" component={Feed} />
-        <Stack.Screen name="New" component={New} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Feed} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
+/*import * as React from "react";
+import { View, Text, Button, LogoTitle } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import New from "../src/pages/New";
+import Feed from "./pages/Feed";
+const Stack = createNativeStackNavigator();
+
+function Myroutes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Feed}
+        options={{ title: "My home" }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default Myroutes;*/
